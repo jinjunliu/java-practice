4e0d4f3dbf48aa5b21c5fd77adc4071ca0d04365
Jinjun Liu <ljj7129@gmail.com>
2022-08-12 18:10:52 -0500

    Update GetField.java

--- JavaReflection/src/GetField.java
+++ JavaReflection/src/GetField.java
@@ -1,20 +1,48 @@
+import java.lang.reflect.Field;
+
 public class GetField {
     public static void main(String[] args) throws Exception {
-        Class stdClass = Student.class;
+        Class<Student> stdClass = Student.class;
         // 获取public字段"score":
         System.out.println(stdClass.getField("score"));
         // 获取继承的public字段"name":
         System.out.println(stdClass.getField("name"));
         // 获取private字段"grade":
         System.out.println(stdClass.getDeclaredField("grade"));
+
+        Field field = String.class.getField("CASE_INSENSITIVE_ORDER");
+        System.out.println(field.getName());
+        System.out.println(field.getType());
+        System.out.println(field.getModifiers());
+
+        Person p = new Person("xiao ming");
+        System.out.println(p.getName()); // "Xiao Ming"
+        Class c = p.getClass();
+        Field f = c.getDeclaredField("name");
+        f.setAccessible(true);
+        f.set(p, "Xiao Hong");
+        System.out.println(p.getName()); // "Xiao Hong"
     }
 
-    class Student extends Person {
-        public int score;
-        private int grade;
+}
+
+
+class Student extends Person {
+    public int score;
+    private int grade;
+
+    public Student(String name) {
+        super(name);
+    }
+}
+
+class Person {
+    public String name;
+    public Person(String name) {
+        this.name = name;
     }
 
-    class Person {
-        public String name;
+    public String getName() {
+        return name;
     }
 }
