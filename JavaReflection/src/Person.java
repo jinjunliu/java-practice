5db68707f126babb2d9c6d2b6b19128b75ec3799
Jinjun Liu <ljj7129@gmail.com>
2022-08-12 19:41:17 -0500

    Create Person.java

---
+++ JavaReflection/src/Person.java
@@ -0,0 +1,14 @@
+class Person {
+    public String name;
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+}
